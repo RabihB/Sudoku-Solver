102ab938702e6efc5c7b1c903ee834d7877e3518
Rabih Bader <121131076+RabihB@users.noreply.github.com>
2023-01-26 23:24:53 +0200

    Add files via upload

---
+++ Sudokusolver_basic.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import numpy as np
+
+grid = [
+[4,0,0,0,0,5,0,0,0],
+[0,0,0,0,0,0,1,9,8],
+[3,0,0,0,8,2,4,0,0],
+[0,0,0,1,0,0,0,8,0],
+[9,0,3,0,0,0,0,0,0],
+[0,0,0,0,3,0,6,7,0],
+[0,5,0,0,0,9,0,0,0],
+[0,0,0,2,0,0,9,0,7],
+[6,4,0,3,0,0,0,0,0]]
+
+def possible(x,y,n):
+    for i in range(9):
+        if grid[y][i] == n:
+            return False
+    
+    for i in range(9):
+        if grid[i][x] == n:
+            return False
+
+    x0 = (x//3)*3
+    y0 = (y//3)*3
+    for i in range(3):
+        for j in range(3):
+            if grid[y0+i][x0+i] ==n:
+                return False
+    return True
+
+def solve():
+    global grid
+    for y in range(9):
+        for x in range(9):
+            if grid[y][x] == 0:
+                for n in range(1,10):
+                    if possible(x,y,n):
+                        grid[y][x] = n
+                        solve()
+                        grid[y][x] = 0
+                return
+    print(np.matrix(grid))
+    input("More solutions?") 
+
+solve()                   
+
+
+
+
+
+
+
+
+
+
